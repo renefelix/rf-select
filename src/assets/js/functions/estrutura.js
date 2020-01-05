8f82a4045fd6a8179bf1b378d6091c6f57baae0d
Rene Felix <rennebf@gmail.com>
2020-01-05 01:58:22 -0300

    JS - Estrutura do select

---
+++ src/assets/js/functions/estrutura.js
@@ -0,0 +1,7 @@
+export default function estruturaPrincipal(placeholder){
+   return `<div class="rf-select" tabindex="0" role="listbox">
+        <div class="rf-select-header">
+            ${placeholder}
+        </div>
+   </div>`;
+}
