1ed2e0db00aeffea2d4fb3175c1b04d299f1c88b
Rene Felix <rennebf@gmail.com>
2020-01-05 03:45:39 -0300

    Aplicação do select

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,7 @@
 import '../scss/index.scss';
 import estruturaPrincipal from './functions/estrutura';
 import placeholder from './functions/placeholder';
+import cliqueNoSelect from './functions/clique-select';
 
 /**
  * Estrutura principal do select
@@ -10,20 +11,23 @@ import placeholder from './functions/placeholder';
 function rfSelect(classSelect, args = false){
     let selects = document.querySelectorAll(classSelect);
 
-    Array.prototype.forEach.call(selects, (select, index) => {
+    Array.prototype.forEach.call(selects, (select, indexSelect) => {
         /**
          * Esconde todos os selects
          */
-        // select.setAttribute('hidden','hidden');
+        select.setAttribute('hidden','hidden');
 
         /**
          * Pega todos os selects e cria a estrutura inicial do HMTL
          * antes de iniciar a TAG
          */
         select.insertAdjacentHTML('beforebegin', estruturaPrincipal(
-            placeholder(select)
+            placeholder(select),
+            indexSelect
         ));
     });
+
+    cliqueNoSelect(classSelect);
 }
 
 
