03c9468b314f9b1a0f24737ef6f0d122dfd50852
Rene Felix <rennebf@gmail.com>
2020-01-05 01:58:10 -0300

    Função principal

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,30 @@
 import '../scss/index.scss';
+import estruturaPrincipal from './functions/estrutura';
+import placeholder from './functions/placeholder';
+
+/**
+ * Estrutura principal do select
+ * @param {string} classSelect nome da classe do elemento que vai ser estilizada
+ * @param {Array} args Array de argumentos para personalização do select
+ */
+function rfSelect(classSelect, args = false){
+    let selects = document.querySelectorAll(classSelect);
+
+    Array.prototype.forEach.call(selects, (select, index) => {
+        /**
+         * Esconde todos os selects
+         */
+        // select.setAttribute('hidden','hidden');
+
+        /**
+         * Pega todos os selects e cria a estrutura inicial do HMTL
+         * antes de iniciar a TAG
+         */
+        select.insertAdjacentHTML('beforebegin', estruturaPrincipal(
+            placeholder(select)
+        ));
+    });
+}
+
+
+rfSelect('.select');
