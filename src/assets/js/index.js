3c3dd453f0e6b84b91f70110c71ce7fe03f3fc51
Rene Felix <rennebf@gmail.com>
2020-01-05 19:45:11 -0300

    Refatoração do código principal

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,6 +2,35 @@ import '../scss/index.scss';
 import estruturaPrincipal from './functions/estrutura';
 import placeholder from './functions/placeholder';
 import cliqueNoSelect from './functions/clique-select';
+import cliqueForaDoSelect from './functions/clique-fora';
+
+function montagemDoSelect(select, indexSelect){
+    /**
+     * Verifica se o select já esta aplicado para atualizar o dkt-select
+     * quando o for chamado novamente a função e não duplicar
+     */
+    let rfSelectSibling = select.previousElementSibling;
+
+    if(rfSelectSibling != null){
+        if(rfSelectSibling.classList.contains('rf-select') === true){
+            rfSelectSibling.remove();
+        }
+    }
+
+    /**
+     * Esconde todos os selects
+     */
+    select.setAttribute('hidden','hidden');
+
+    /**
+     * Pega todos os selects e cria a estrutura inicial do HMTL
+     * antes de iniciar a TAG
+     */
+    select.insertAdjacentHTML('beforebegin', estruturaPrincipal(
+        placeholder(select),
+        indexSelect
+    ));
+}
 
 /**
  * Estrutura principal do select
@@ -12,22 +41,11 @@ function rfSelect(classSelect, args = false){
     let selects = document.querySelectorAll(classSelect);
 
     Array.prototype.forEach.call(selects, (select, indexSelect) => {
-        /**
-         * Esconde todos os selects
-         */
-        select.setAttribute('hidden','hidden');
-
-        /**
-         * Pega todos os selects e cria a estrutura inicial do HMTL
-         * antes de iniciar a TAG
-         */
-        select.insertAdjacentHTML('beforebegin', estruturaPrincipal(
-            placeholder(select),
-            indexSelect
-        ));
+        montagemDoSelect(select, indexSelect);
     });
 
     cliqueNoSelect(classSelect);
+    cliqueForaDoSelect();
 }
 
 
