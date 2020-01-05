693836b9ff49b7735dcedcfcbcdb40906940bf64
Rene Felix <rennebf@gmail.com>
2020-01-05 03:44:48 -0300

    Aumento da estrutura principal

--- src/assets/js/functions/estrutura.js
+++ src/assets/js/functions/estrutura.js
@@ -1,7 +1,17 @@
-export default function estruturaPrincipal(placeholder){
+export default function estruturaPrincipal(placeholder, indexSelect){
    return `<div class="rf-select" tabindex="0" role="listbox">
         <div class="rf-select-header">
             ${placeholder}
         </div>
+
+        <div class="rf-listagem">
+			<div class="rf-select-busca">
+				<input class="rf-select-input-busca" type="text" value="" placeholder="Buscar" id="rf-select-buscar-${indexSelect}" data-rf-select="${indexSelect}" />
+			</div>
+
+			<div class="rf-scroll">
+				<ul class="rf-ul-listagem" role="list"></ul>
+			</div>
+		</div>
    </div>`;
 }
