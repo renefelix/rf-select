4b907726918925437f8edf26282329bf781d73e5
Rene Felix <rennebf@gmail.com>
2020-01-05 19:44:41 -0300

    Correção quando há mais de um option selected no select simples

--- src/assets/js/functions/listagem-options.js
+++ src/assets/js/functions/listagem-options.js
@@ -1,3 +1,5 @@
+import { contagemDeOptionsSelecionados, indexOpcaoSelecionada } from "./conta-options-selecionados";
+
 /**
  * Função responsável por criar a listagem de options de cada select
  */
@@ -24,13 +26,32 @@
 			let textDoOption = opcao.text;
 			let disabled = opcao.getAttribute('disabled');
 
-			ulListagem[indexSelect].insertAdjacentHTML('beforeend', 
-			`<li tabindex="0" class="rf-select-option rf-select-option-${indexSelect} ${disabled != null ? 'disabled' : ''} ${opcao.hasAttribute('selected') ? 'selected' : ''}" data-rf-select="${indexSelect}" data-rf-option="${i}" role="listitem">
-				${textDoOption}
-			</li>`);
+            ulListagem[indexSelect].insertAdjacentHTML('beforeend', 
+            `<li tabindex="0" class="rf-select-option rf-select-option-${indexSelect} ${disabled != null ? 'disabled' : ''} ${opcao.hasAttribute('selected') ? 'selected' : ''}" data-rf-select="${indexSelect}" data-rf-option="${i}" role="listitem">
+                ${textDoOption}
+            </li>`);
 		}
 	}else{
 		ulListagem[indexSelect].innerHTML = `<li class="rf-select-empty">Nenhum resultado</li>`;
-	}
+    }
 
+    /**
+     * Função que verifica se o select não é multiplo mas possui
+     * mais de uma option selecionada, porque se tiver, marca apenas
+     * a ultima opcao do RF Select como selecionada
+     */
+    if(selectDOMReferente.hasAttribute('multiple') === false){
+        if(contagemDeOptionsSelecionados(selectDOMReferente) >= 2){
+
+            let rfOpcoes = document.querySelectorAll(`li[data-rf-select="${indexSelect}"]`);
+
+            // Remove a class de selected de todas as opções 
+            Array.prototype.forEach.call(rfOpcoes, (rfOpcao) => {
+                rfOpcao.classList.remove('selected');
+            });
+
+            // Adiciona a class selected apenas na opção clicada
+            rfOpcoes[indexOpcaoSelecionada(selectDOMReferente)].classList.add('selected');
+        }
+    }
  }
